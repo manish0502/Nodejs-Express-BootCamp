2296b22a3a26355b5b5004fd9b72a5d3b57d3ed6
manish0502 <manishgoswami495@gmail.com>
2021-10-17 22:40:38 +0530

    something changes-2

--- project-4/routes/route.js
+++ project-4/routes/route.js
@@ -1,29 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { register  ,login} = require('../controllers/auth')
+const { register, login } = require("../controllers/auth");
 
+router.route("/register")
+           .post(register);
 
-
-// router.route('/dashboard')
-//         .get(authMiddleware, dashboard)
-
-router.route('/register')
-            .post(register)
-            
-router.route('/login')
-            .post(login)
-            
-
-
-
-router.get('/' ,(req, res) => {
-    res.json({
-        msg:"Hello from jobsAPI",
-        status: 200
-    })
-})
-
-
+router.route("/login")
+            .post(login);
 
 
 
